d474e28f85b5b9e49bedad701ec8bc05660178c8
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-23 15:58:22 +0200

    Modification de la base pour intégrer le compte de destination en cas de virement

--- sql/Bank.sql
+++ sql/Bank.sql
@@ -9,20 +9,23 @@ USE Bank;
 -- - Construction des TABLESPACE						                     ---
 -- -----------------------------------------------------------------------------
 CREATE TABLE Bank_Account (
-	NumBankAccount		varchar(12) 	PRIMARY KEY,
-	Holder				varchar(30)		NOT NULL,
-	Balance 			DECIMAL(10,2) 	NOT NULL DEFAULT 0,
-	MaximumBalance		decimal(10,2)	NOT NULL
+	NumBankAccount			varchar(12) 	PRIMARY KEY,
+	Holder					varchar(30)		NOT NULL,
+	Balance 				DECIMAL(10,2) 	NOT NULL DEFAULT 0,
+	MaximumBalance			decimal(10,2)	NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE Operations(
-	IdOperation 		int(4) 			PRIMARY KEY AUTO_INCREMENT,
-	DateOperation 		DATETIME 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	AmountTransaction	decimal(10,2)	NOT NULL,
-	TypeOperation		smallint		NOT NULL,
-	NumBankAccount      varchar(12) 	NOT NULL,
-	KEY NumBankAccount (NumBankAccount)
+	IdOperation 			int(4) 			PRIMARY KEY AUTO_INCREMENT,
+	DateOperation 			DATETIME 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	AmountTransaction		decimal(10,2)	NOT NULL,
+	TypeOperation			smallint		NOT NULL,
+	NumBankAccount      	varchar(12) 	NOT NULL,
+	NumBankAccountDestination varchar(12) 	NULL,
+	KEY NumBankAccount (NumBankAccount),
+	KEY NumBankAccountDestination (NumBankAccountDestination)
  ) ENGINE = InnoDB;
 
 ALTER TABLE Operations
-  ADD CONSTRAINT `operations_ibfk_1` FOREIGN KEY (`NumBankAccount`) REFERENCES `Bank_Account` (`NumBankAccount`);
+  ADD CONSTRAINT `operations_ibfk_1` FOREIGN KEY (`NumBankAccount`) REFERENCES `Bank_Account` (`NumBankAccount`),
+  ADD CONSTRAINT `operations_ibfk_2` FOREIGN KEY (`NumBankAccountDestination`) REFERENCES `Bank_Account` (`NumBankAccount`)
