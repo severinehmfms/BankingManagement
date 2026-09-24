b1b28058f42881a9f865ee20f5e2ea3824c2964d
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-24 10:42:45 +0200

    Ajout getters et setters, méthode abstraite getTypeOperation

--- src/fr/projetbank/models/Operation.java
+++ src/fr/projetbank/models/Operation.java
@@ -1,19 +1,83 @@
 package fr.projetbank.models;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
-public class Operation {
+public abstract class Operation {
 	
 	/**
 	 * Opération bancaire
 	 */
 	
-	public static enum TypeTransaction {
+	public static enum typesOperations {
 		DEPOSIT, WITHDRAWAL, TRANSFER
 	}
 	
-	private Date dateTransaction;
-	private double amountTransaction;
+	private int idOperation;
+	private Date dateOperation;
+	private BigDecimal amountTransaction;
 	private BankAccount bankAccount;
+	
+	/**
+	 * Constructeur quand l'identifiant n'est pas encore connu
+	 * @param dateOperation
+	 * @param amountTransaction
+	 * @param bankAccount
+	 */
+	public Operation(Date dateOperation, BigDecimal amountTransaction, BankAccount bankAccount) {
+		this.idOperation = 0;
+		this.dateOperation = dateOperation;
+		this.amountTransaction = amountTransaction;
+		this.bankAccount = bankAccount;
+	}
+	
+	/**
+	 * Constructeur quand l'identifiant est connu
+	 * @param identifiant
+	 * @param dateOperation
+	 * @param amountTransaction
+	 * @param bankAccount
+	 */
+	public Operation(int idOperation, Date dateOperation, BigDecimal amountTransaction, BankAccount bankAccount) {
+		this.idOperation = idOperation;
+		this.dateOperation = dateOperation;
+		this.amountTransaction = amountTransaction;
+		this.bankAccount = bankAccount;
+	}	
 
+	public int getIdOperation() {
+		return idOperation;
+	}
+
+	public void setIdOperation(int idOperation) {
+		this.idOperation = idOperation;
+	}
+
+	public Date getDateOperation() {
+		return dateOperation;
+	}
+	public void setDateOperation(Date dateOperation) {
+		this.dateOperation = dateOperation;
+	}
+	public BigDecimal getAmountTransaction() {
+		return amountTransaction;
+	}
+	public void setAmountTransaction(BigDecimal amountTransaction) {
+		this.amountTransaction = amountTransaction;
+	}
+	public BankAccount getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	//Permet à chaque classe fille de connaître le type d'opération
+	public abstract typesOperations getTypeOperation();
+	
+	//Renvoie null si la méthode est pas redéfinie par la classe fille (pour la classe fille Transfer elle ne renverra pas null)
+	public String getNumBankAccountDestination() {
+	    return null;
+	}
+	
 }
