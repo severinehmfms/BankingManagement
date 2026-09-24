02d47a6a96a63c142b076edb499b0dca5e717b72
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-24 10:43:22 +0200

    Redéfinition de la méthode abstraite getTypeOperation pour Deposit

--- src/fr/projetbank/models/Deposit.java
+++ src/fr/projetbank/models/Deposit.java
@@ -1,9 +1,20 @@
 package fr.projetbank.models;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Deposit extends Operation{
-	
+
 	/**
 	 * Dépôt
 	 */
 
+	public Deposit(Date dateOperation, BigDecimal amountTransaction, BankAccount bankAccount) {
+		super(dateOperation, amountTransaction, bankAccount);
+	}
+	
+	@Override
+    public typesOperations getTypeOperation() {
+        return typesOperations.DEPOSIT;
+    }
 }
