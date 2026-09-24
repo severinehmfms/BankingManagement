7c5388bb8edb2a287f5dec8cbcdd599cfe7c55d2
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-24 10:43:38 +0200

    Redéfinition de la méthode abstraite getTypeOperation pour Withdrawal

--- src/fr/projetbank/models/Withdrawal.java
+++ src/fr/projetbank/models/Withdrawal.java
@@ -1,9 +1,23 @@
 package fr.projetbank.models;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import fr.projetbank.models.Operation.typesOperations;
+
 public class Withdrawal extends Operation{
+
+	
 	
 	/**
 	 * Retrait
 	 */
-
+	public Withdrawal(Date dateOperation, BigDecimal amountTransaction, BankAccount bankAccount) {
+		super(dateOperation, amountTransaction, bankAccount);
+	}
+	
+	@Override
+    public typesOperations getTypeOperation() {
+        return typesOperations.WITHDRAWAL;
+    }
 }
