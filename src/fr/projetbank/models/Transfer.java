f8041f553d77062b15b6729b3d1431410be31395
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-24 10:43:50 +0200

    Redéfinition de la méthode abstraite getTypeOperation pour Transfer

--- src/fr/projetbank/models/Transfer.java
+++ src/fr/projetbank/models/Transfer.java
@@ -1,5 +1,10 @@
 package fr.projetbank.models;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import fr.projetbank.models.Operation.typesOperations;
+
 public class Transfer extends Operation{
 	
 	/**
@@ -8,6 +13,26 @@ public class Transfer extends Operation{
 	
 	private BankAccount bankAccountDestination;
 	
+	public Transfer(Date dateOperation, BigDecimal amountTransaction, BankAccount bankAccount, BankAccount bankAccountDestination) {
+		super(dateOperation, amountTransaction, bankAccount);
+		this.bankAccountDestination = bankAccountDestination;
+	}
+
+	@Override
+    public typesOperations getTypeOperation() {
+        return typesOperations.TRANSFER;
+    }
 	
+	public BankAccount getBankAccountDestination() {
+		return bankAccountDestination;
+	}
+
+	public void setBankAccountDestination(BankAccount bankAccountDestination) {
+		this.bankAccountDestination = bankAccountDestination;
+	}	
 	
+	@Override
+	public String getNumBankAccountDestination() {
+	    return bankAccountDestination.getNumBankAccount();
+	}
 }
